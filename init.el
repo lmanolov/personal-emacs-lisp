98752a9afd12cfa7ee4a4183f8c1cebc82227651
Luben Manolov <lubo@manolov.org>
2008-09-30 11:25:31 +0300

    Added color-theme

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
 (add-to-list 'load-path "~/elisp/ruby")
 (add-to-list 'load-path "~/elisp/semantic")
 (add-to-list 'load-path "~/elisp/speedbar")
+(add-to-list 'load-path "~/elisp/color-theme")
 
 
 ;; ********************************************************************************
@@ -30,6 +31,7 @@
 (require 'tabbar)
 (require 'cedet)
 (require 'ecb)
+(require 'color-theme)
 
 ;; ********************************************************************************
 ;; Load my own elisp stuff
@@ -586,96 +588,6 @@
 		       (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
 
 
-
-;; ********************************************************************************
-;; Faces
-;;
-
-(set-face-attribute 'default nil 
-		    :foreground "#333"
-		    :background "#ffffff")
-
-(set-face-attribute 'hl-line nil 
-		    :background "#ffc")
-
-(set-face-attribute 'fringe nil 
-		    :background "#fff")
-
-(set-face-attribute 'cursor nil
-		    :background "#000")
-
-(set-face-attribute 'dropdown-list-face nil  
-		    :foreground "#000"
-		    :background "#eef")
-
-(set-face-attribute 'dropdown-list-selection-face nil
-		    :background "#fcc")
-
-(set-face-attribute 'vertical-border nil 
-		    :foreground "#eee")
-
-(set-face-attribute 'region nil
-		    :background "#fcc")
-
-(set-face-attribute 'mode-line nil 
-		    :foreground "#666" 
-		    :background "#eee"
-		    :box '(:line-width 1 :color "#eee"))
-
-(set-face-attribute 'mode-line-inactive nil 
-		    :foreground "#999" 
-		    :background "#fff"
-		    :box '(:line-width 1 :color "#fff"))
-
-(set-face-attribute 'mode-line-buffer-id nil
-		    :foreground "#111")
-
-(set-face-attribute 'mode-line-highlight nil 
-		    :foreground "#333" 
-		    :background "#ccc" 
-		    :box '(:line-width 1 :color "#444"))
-
-(set-face-attribute 'tabbar-default nil
-		    :height 1.0
-		    :foreground "#333" 
-		    :background "#fff"
-		    :inherit 'default)
-
-(set-face-attribute 'tabbar-selected nil
-		    :foreground "#0af" 
-		    :background "#e9e9e9"
-		    :weight 'bold
-		    :box '(:line-width 1 :color "#fff" :style released-button))
-
-(set-face-attribute 'tabbar-unselected nil
-		    :foreground "#666"
-		    :box '(:line-width 1 :color "#fff"))
-
-(set-face-attribute 'tabbar-button nil
-		    :foreground "#666"
-		    :box '(:line-width 1 :color "#fff"))
-
-(set-face-attribute 'font-lock-builtin-face nil
-		    :foreground "#909")
-
-(set-face-attribute 'font-lock-keyword-face nil
-		    :foreground "#009")
-
-(set-face-attribute 'font-lock-function-name-face nil
-		    :foreground "#090")
-
-(set-face-attribute 'font-lock-variable-name-face nil
-		    :foreground "#099")
-
-(set-face-attribute 'font-lock-comment-face nil
-		    :foreground "#c90")
-
-(set-face-attribute 'font-lock-string-face nil
-		    :foreground "#c93")
-
-
-
-
 (defun tab-label (tab)
   (if tabbar--buffer-show-groups
       (format " [%s] " (tabbar-tab-tabset tab))
@@ -788,3 +700,29 @@
 ;; Start emacs server
 (server-start)
 
+
+;; ********************************************************************************
+;; Color theme
+(color-theme-initialize)
+(load-file "~/elisp/color-theme-blue.el")
+(color-theme-blue)
+
+(set-face-attribute 'tabbar-default nil
+		    :height 1.0
+		    :foreground "#333" 
+		    :background "#fff"
+		    :inherit 'default)
+
+(set-face-attribute 'tabbar-selected nil
+		    :foreground "#0af" 
+		    :background "#e9e9e9"
+		    :weight 'bold
+		    :box '(:line-width 1 :color "#fff" :style released-button))
+
+(set-face-attribute 'tabbar-unselected nil
+		    :foreground "#666"
+		    :box '(:line-width 1 :color "#fff"))
+
+(set-face-attribute 'tabbar-button nil
+		    :foreground "#666"
+		    :box '(:line-width 1 :color "#fff"))
